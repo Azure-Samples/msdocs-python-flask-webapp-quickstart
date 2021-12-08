c14e5a642583cfb937570b8e29ed8a34db547533
David Berry <dberry83702@yahoo.com>
2021-12-08 15:16:48 -0600

    Added logging statements

--- src/app.py
+++ src/app.py
@@ -4,16 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():   
+def index():
+   print('Request for index page received')
    return render_template('index.html')
 
 
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
+
    if name:
+       print('Request for hello page received with name=%s' % name)
        return render_template('hello.html', name = name)
    else:
+       print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
 
