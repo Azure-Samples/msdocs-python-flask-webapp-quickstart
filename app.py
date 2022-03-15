a5120084500abedfdc7b45c67ee2729f17bd7285
nobel-2004 <101637148+nobel-2004@users.noreply.github.com>
2022-03-15 18:21:41 +0530

    Second Commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
 
    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+       return render_template('Welcome to Persistent.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
