9af48b4d2cff70586e63d18d236bfb09284f595d
vmagelo <v-gelorminom@microsoft.com>
2023-04-24 13:30:01 +0200

    For container testing.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.11
+
+WORKDIR /code
+
+COPY requirements.txt .
+
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+EXPOSE 50505
+
+ENTRYPOINT ["gunicorn", "app:app"]
---
+++ gunicorn.conf.py
@@ -0,0 +1,14 @@
+# Gunicorn configuration file
+import multiprocessing
+
+max_requests = 1000
+max_requests_jitter = 50
+
+log_file = "-"
+
+bind = "0.0.0.0:50505"
+
+workers = (multiprocessing.cpu_count() * 2) + 1
+threads = workers
+
+timeout = 120
