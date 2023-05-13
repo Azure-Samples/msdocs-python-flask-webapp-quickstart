27197162f7b389e74c479d26c93fc1b54bd12427
Lasse Willaume Pedersen <14995@student.eadania.dk>
2023-05-13 21:20:22 +0200

    added route for upload

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Handle the image data sent from the Maui application
+    image = request.files['file']
+    image_bytes = image.read()
+
+    # Do something with the image data (e.g. save it to disk, process it, etc.)
+    # ...
+
+    # Return a response to the Maui application
+    return 'Image received!'
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
