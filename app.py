6cce6e3f0412b739ca8c78957c3f74e50d3fc366
conpandiyant <160731386+conpandiyant@users.noreply.github.com>
2024-02-21 23:00:47 +0530

    added azure upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import ContentSettings
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
@@ -25,6 +26,28 @@ def hello():
            }
    df = pd.DataFrame(data)
    df.to_csv('push.csv', mode='a', index=False, header=False)
+
+
+   block_blob_service = BlockBlobService(account_name='storagesaphanatestdl',
+                                         account_key='vsqZoXBJW3SRTROGAfkyRuLW6egrlCjK/vOFtiShaawzwYhXarYbmHqBKxluOSjmfyJY88rgpFgs+AStUsxu+w==')
+   block_blob_service.create_container('testdata')
+
+   # Upload the CSV file to Azure cloud
+   block_blob_service.create_blob_from_path(
+       'testdata',
+       'testdata.csv',
+       'push.csv',
+       content_settings=ContentSettings(content_type='application/CSV')
+   )
+
+   # Check the list of blob
+   generator = block_blob_service.list_blobs('testdata')
+   for blob in generator:
+       print(blob.name)
+
+   # Download the CSV file From Azure storage
+   block_blob_service.get_blob_to_path('testdata', 'testdata.csv', 'push.csv')
+
 
    if name:
        print('Request for hello page received with name=%s' % name)
