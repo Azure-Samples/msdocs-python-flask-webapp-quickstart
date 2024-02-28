bf95a8cbaf98f608453d5b21fd4b0afcc438af58
conpandiyant <160731386+conpandiyant@users.noreply.github.com>
2024-02-28 10:42:26 +0530

    fix233

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index1.html')
+   return render_template('index.html')
 
 
 
