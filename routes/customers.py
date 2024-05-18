0f0c1eaba5757001f049dbe4ac2cf011f763310e
DaiAustinYersin <dainguyen4869@gmail.com>
2024-05-18 18:46:22 +0700

    provide blueprints to list employees and customers

--- routes/customers.py
+++ routes/customers.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from app import cursor
 from routes import bp_ctm
 
 
 @bp_ctm.route('/')
 def index():
-    return render_template('customers/index.html')
+    customers = cursor.execute("SELECT * FROM customers")
+    return render_template('customers/index.html', customers=customers)
--- routes/employees.py
+++ routes/employees.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from app import cursor
 from routes import bp_emp
 
 
 @bp_emp.route('/')
 def index():
-    return render_template('employees/index.html')
+    employees = cursor.execute("SELECT * FROM employees")
+    return render_template('employees/index.html', employees=employees)
