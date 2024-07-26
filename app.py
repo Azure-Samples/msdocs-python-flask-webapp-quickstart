496a301ebf32c3932fbda1c6f8aded4ef7139a6a
pshah16 <pshah@equinor.com>
2024-07-26 01:08:03 +0000

    fixed port
    fixed port

--- app.py
+++ app.py
@@ -200,11 +200,10 @@ def hello():
        return redirect(url_for('index'))
 
 
-# if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+   app.run()
 
-if __name__ == "__main__":
-    # init the global index
-    initialize_index()
+# if __name__ == "__main__":
+#     # init the global index
 
-    app.run(host="0.0.0.0", port=5601)
+#     app.run(host="0.0.0.0", port=5601)
