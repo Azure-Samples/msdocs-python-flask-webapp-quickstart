657caf5ac57fa0877d8c0506c6bcf5d46812e954
Ritik Singh <riik.kr.singh102000@gmail.com>
2024-12-16 12:21:12 +0530

    my-test

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def hello():
    name = request.form.get('name')
 
    if name:
-       print('Request for hello page received with name=%s' % name)
+       print('Request for hello page received with name not surname=%s' % name)
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
