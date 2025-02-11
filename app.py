9d6a20d72d3f9682e2af08b37c193f845a6f3964
Yang Yang <yang.yang@databricks.com>
2025-02-10 22:02:47 -0800

    add print package list lines

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-import os
+#: check all installed packages.
+import pkg_resources
+installed_packages = [pkg.key for pkg in pkg_resources.working_set]
+print(installed_packages)
 
+
+import os
 # import openai
 # from langchain_openai import AzureOpenAI
 from azure.identity import DefaultAzureCredential
