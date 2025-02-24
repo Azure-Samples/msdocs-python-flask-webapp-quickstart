fe9787d121eb8910276f00d5edd5604ef72c5d10
Christoph <christoph.aublinger@oilfielddata.com>
2025-02-24 20:04:14 +0100

    display in window

--- utility.py
+++ utility.py
@@ -5,5 +5,4 @@ import plotly.express as px
 def get_plotly_plot():
     wide_df = px.data.medals_wide()
     figure = px.bar(wide_df, x="nation", y=["gold", "silver", "bronze"], title="Wide-Form Input")
-    figure.show()
     return json.dumps(figure, cls=plotly.utils.PlotlyJSONEncoder)
