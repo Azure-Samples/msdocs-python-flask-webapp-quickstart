96b54c06253bdcf5b6eafc72a499cd9a43b53db9
Christoph <christoph.aublinger@oilfielddata.com>
2025-02-26 09:29:30 +0100

    one page

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('index.html', graphJSON=get_plotly_plot_wisam())
 
 @app.route('/favicon.ico')
 def favicon():
